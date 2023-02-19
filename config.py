1e45adf233a7931a4011554c7c07e7a9f8a4de53
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-19 22:28:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,66 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+
+DB_URI = os.environ.get("DATABASE_URL", "")
+
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+
+
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+
+if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
+    DISABLE_CHANNEL_BUTTON = True
+else:
+    DISABLE_CHANNEL_BUTTON = False
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(1250450587)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
